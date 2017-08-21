a071ae5e2ffb4c5de5ba5c5ce351e7ffdeb4e4b9
Viktor Hansson <viktor.hansson@me.com>
2017-08-21 11:06:19 +0200

    Updated with centos/ubuntu images plus salt 2016.11

--- start.sh
+++ start.sh
@@ -2,17 +2,32 @@
 
 # Variables from environement
 : "${SALT_USE:=master}"
-: "${SALT_NAME:=master}"
+: "${SALT_NAME:=$(hostname)}"
 : "${LOG_LEVEL:=info}"
 : "${OPTIONS:=}"
+
+if hash yum 2>/dev/null ; then
+	OS=centos
+elif hash apt-get 2>/dev/null ; then
+	OS=ubuntu
+else
+	echo "Neither ubuntu nor centos"
+	exit 1
+fi
 
 # Set minion id
 echo $SALT_NAME > /etc/salt/minion_id
 
 # If salt master also start minion in background
 if [ $SALT_USE == "master" ]; then
+  /usr/bin/salt-key -D
   echo "INFO: Starting salt-minion and auto connect to salt-master"
-  sudo service salt-minion start
+  if [[ ${OS} == "ubuntu" ]]; then
+  	service salt-minion start
+  elif [[ ${OS} == "centos" ]]; then
+  	/usr/bin/salt-minion --log-level=$LOG_LEVEL $OPTIONS &
+  	echo "INFO: Started salt-minion with pid: $!"
+  fi
 fi
 
 # Set salt grains
@@ -23,4 +38,4 @@ fi
 
 # Start salt-$SALT_USE
 echo "INFO: Starting salt-$SALT_USE with log level $LOG_LEVEL with hostname $SALT_NAME"
-sudo /usr/bin/salt-$SALT_USE --log-level=$LOG_LEVEL $OPTIONS
+/usr/bin/salt-$SALT_USE --log-level=$LOG_LEVEL $OPTIONS
